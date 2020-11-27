bfb4c8021df6279b3b8d55aa2aeb3c797746711e
Puneet Behl <behlp@objectcomputing.com>
2020-11-28 03:23:44 +0530

    Fix Project
    * Correct ES Version
    * Configure ES Settings in the application.yml
    * Cleanup Commented code

--- grails-app/conf/application.groovy
+++
@@ -1,8 +0,0 @@
-elasticSearch {
-    // see http://noamt.github.io/elasticsearch-grails-plugin/guide/configuration.html for defaults
-    client.mode = "transport"
-    client.hosts = [[host: "localhost", port: 9200]]
-    datastoreImpl = "mongoDatastore"
-    bulkIndexOnStartup = false
-    migration.strategy = 'deleteIndex'
-}
